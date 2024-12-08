bfbc158ca6e2f744ec8c536794de616e95989554
Sai Hitesh <99950365+hitesh-vs@users.noreply.github.com>
2024-12-08 12:16:48 +0530

    Update task1.cs

--- integrated/task1.cs
+++ integrated/task1.cs
@@ -32,6 +32,10 @@ public class task1 : Agent
     private Vector3 initialEndEffectorPosition = new Vector3(3.5f, 3.5f, -3.05f);
     private Quaternion initialEndEffectorRotation = new Quaternion(0, 0, 0, 1);
 
+    private Vector3 finalArm1Position = new Vector3(2f, 0.4f, 0f);
+    private Vector3 finalArm2Position = new Vector3(2f, 0.7f, 0.2f);
+    private Vector3 finalEndEffectorPosition = new Vector3(2f, 0.7f, 0.45f);
+
     //private Vector3 initialTargetPosition;
     //private Quaternion initialTargetRotation;
 
@@ -55,83 +59,103 @@ public class task1 : Agent
 
     public override void OnEpisodeBegin()
     {
-
-        // Reset arm1
-        arm1.transform.position = initialArm1Position;
-        arm1.transform.rotation = initialArm1Rotation;
-
-        //Rigidbody arm1Rb = arm1.GetComponent<Rigidbody>();
-        if (arm1 != null)
+        if (!task1done)
         {
-            arm1.linearVelocity = Vector3.zero;
-            arm1.angularVelocity = Vector3.zero;
-            arm1.useGravity = false; // Disable gravity
-            arm1.constraints = RigidbodyConstraints.FreezePositionY; // Freeze necessary axes
+            // Reset arm1
+            arm1.transform.position = initialArm1Position;
+            arm1.transform.rotation = initialArm1Rotation;
+
+            //Rigidbody arm1Rb = arm1.GetComponent<Rigidbody>();
+            if (arm1 != null)
+            {
+                arm1.linearVelocity = Vector3.zero;
+                arm1.angularVelocity = Vector3.zero;
+                arm1.useGravity = false; // Disable gravity
+                arm1.constraints = RigidbodyConstraints.FreezePositionY; // Freeze necessary axes
+            }
+
+            // Reset arm2
+            arm2.transform.position = initialArm2Position;
+            arm2.transform.rotation = initialArm2Rotation;
+
+            //Rigidbody arm2Rb = arm2.GetComponent<Rigidbody>();
+            if (arm2 != null)
+            {
+                arm2.linearVelocity = Vector3.zero;
+                arm2.angularVelocity = Vector3.zero;
+                arm2.useGravity = false; // Disable gravity
+                arm2.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation; // Freeze necessary axes
+            }
+
+            // Reset end-effector
+            endEffector.transform.position = initialEndEffectorPosition;
+            endEffector.transform.rotation = initialEndEffectorRotation;
+
+            //Rigidbody eeRb = endEffector.GetComponent<Rigidbody>();
+            if (endEffector != null)
+            {
+                endEffector.linearVelocity = Vector3.zero;
+                endEffector.angularVelocity = Vector3.zero;
+                endEffector.useGravity = false; // Disable gravity
+                endEffector.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation; // Freeze necessary axes
+
+            }
+
+            //if (dummy != null)
+            //{
+            //    dummy.linearVelocity = Vector3.zero;
+            //    dummy.angularVelocity = Vector3.zero;
+            //    dummy.useGravity = false; // Disable gravity
+            //    dummy.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
+
+            //}
+
+            // Reset Rigidbody velocities
+            mobileBase.linearVelocity = Vector3.zero;
+            mobileBase.angularVelocity = Vector3.zero;
+
+            // Find a safe spawn position
+            Vector3 spawnPosition;
+            spawnPosition = new Vector3(3.5f, 0.05f, -3.5f);
+
+            // Set position and reset orientation
+            mobileBase.transform.localPosition = spawnPosition;
+            mobileBase.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+            // Previous distance
+            previousDistanceToTarget = Vector3.Distance(mobileBase.transform.localPosition, targetPosition.localPosition);
+
+            // Randomize target position within a 5x5 area
+            targetPosition.localPosition = new Vector3(2f, 0.05f, 0.05f);
+            //obs 
+            obs1.transform.localPosition = new Vector3(1f, 0.1f, 2f);
+            obs2.transform.localPosition = new Vector3(1f, 0.1f, -2f);
+            obs3.transform.localPosition = new Vector3(-0.6f, 0.1f, 1f);
+            obs4.transform.localPosition = new Vector3(-0.6f, 0.1f, -1f);
+
+            episodeStartTime = Time.time;
         }
-
-        // Reset arm2
-        arm2.transform.position = initialArm2Position;
-        arm2.transform.rotation = initialArm2Rotation;
-
-        //Rigidbody arm2Rb = arm2.GetComponent<Rigidbody>();
-        if (arm2 != null)
-        {
-            arm2.linearVelocity = Vector3.zero;
-            arm2.angularVelocity = Vector3.zero;
-            arm2.useGravity = false; // Disable gravity
-            arm2.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation; // Freeze necessary axes
-        }
-
-        // Reset end-effector
-        endEffector.transform.position = initialEndEffectorPosition;
-        endEffector.transform.rotation = initialEndEffectorRotation;
-
-        //Rigidbody eeRb = endEffector.GetComponent<Rigidbody>();
-        if (endEffector != null)
-        {
-            endEffector.linearVelocity = Vector3.zero;
-            endEffector.angularVelocity = Vector3.zero;
-            endEffector.useGravity = false; // Disable gravity
-            endEffector.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation; // Freeze necessary axes
-
-        }
-
-        //if (dummy != null)
-        //{
-        //    dummy.linearVelocity = Vector3.zero;
-        //    dummy.angularVelocity = Vector3.zero;
-        //    dummy.useGravity = false; // Disable gravity
-        //    dummy.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotation;
-            
-        //}
-
-        // Reset Rigidbody velocities
-        mobileBase.linearVelocity = Vector3.zero;
-        mobileBase.angularVelocity = Vector3.zero;
-
-        // Find a safe spawn position
-        Vector3 spawnPosition;
-        spawnPosition = new Vector3(3.5f, 0.05f, -3.5f);
-
-        // Set position and reset orientation
-        mobileBase.transform.localPosition = spawnPosition;
-        mobileBase.transform.rotation = Quaternion.Euler(0, 0, 0);
-
-        // Previous distance
-        previousDistanceToTarget = Vector3.Distance(mobileBase.transform.localPosition, targetPosition.localPosition);
-
-        // Randomize target position within a 5x5 area
-        targetPosition.localPosition = new Vector3(2f, 0.05f, 0.05f);
-        //obs 
-        obs1.transform.localPosition = new Vector3(1f, 0.1f, 2f);
-        obs2.transform.localPosition = new Vector3(1f, 0.1f, -2f);
-        obs3.transform.localPosition = new Vector3(-0.6f, 0.1f, 1f);
-        obs4.transform.localPosition = new Vector3(-0.6f, 0.1f, -1f);
-
-        episodeStartTime = Time.time;
+        
 
         if (task1done)
         {
+            if (arm1 != null)
+            {
+                arm1.constraints = RigidbodyConstraints.None; // Remove all constraints
+            }
+
+            if (arm2 != null)
+            {
+                arm2.constraints = RigidbodyConstraints.None; // Remove all constraints
+            }
+
+            if (endEffector != null)
+            {
+                endEffector.constraints = RigidbodyConstraints.None; // Remove all constraints
+            }
+
+            mobileBase.transform.localPosition = new Vector3(2.0f, 0.05f, 0f);
+            NavTarget.gameObject.SetActive(false);
             this.enabled = false; // Prevents further execution
             //NavTarget.SetActive(false);
             Debug.Log("First Nav disabled.");
@@ -209,12 +233,14 @@ public class task1 : Agent
         {
             task1done = true;
             SetReward(1.0f); // High reward for reaching the target
+            Debug.Log("Task1 target reached");
             EndEpisode();
         }
 
         if (TouchesObs()) // Goal reached
         {
             SetReward(-1.0f); // High reward for reaching the target
+            Debug.Log("Touched obs");
             EndEpisode();
         }
         //else
@@ -231,6 +257,7 @@ public class task1 : Agent
         if (IsOutsideBoundingBox())
         {
             AddReward(-1.0f); // Negative reward for leaving the allowed area
+            Debug.Log("Outside Bb");
             EndEpisode(); // End episode if agent leaves the bounding box
         }
 
